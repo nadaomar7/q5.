2f6158d42a5fb79c3265802bc9da7f514c724c42
nadaomar7 <nody.omar710@gmail.com>
2022-11-10 21:38:34 +0200

    Initial commit

---
+++ src/u.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+        public class u {
+            public static void main(String[] args) {
+                try {
+                    java.net.URL url = new java.net.URL(
+                            "http://cs.armstrong.edu/liang/data/Lincoln.txt");
+                    int count = 0;
+                    Scanner input = new Scanner(url.openStream());
+                    while (input.hasNext()) {
+                        // Read words
+                        if (Character.isLetter((input.next()).charAt(0))) {
+                            count++;
+                        }
+                    }
+                    System.out.println(
+                            "Number of words in President Abraham Lincoln's Gettysburg address: " +
+                                    count);
+                }
+                catch (java.net.MalformedURLException ex) {
+                    System.out.println("Invalid URL");
+                }
+                catch (java.io.IOException ex) {
+                    System.out.println("I/0 Errors: no such file");
+                }
+            }
+
+
+}
